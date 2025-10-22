843676098654e2d37db1cacf13a33683d4bca78a
Stijn Ruts <git@stijnruts.be>
2025-10-22 17:49:05 +0200

    Reflex example

--- app/Main.hs
+++ app/Main.hs
@@ -1,17 +1,58 @@
 module Main (main) where
 
-import Control.Monad (forever)
+import Control.Monad (forever, void)
+import Control.Monad.IO.Class (liftIO)
 import qualified Data.ByteString.Char8 as BS
 import Evdev (EventData (..))
 import qualified Evdev
 import qualified Evdev.Codes as Codes
 import qualified Evdev.Uinput as Uinput
-import qualified Reactive.Banana as B
-import qualified Reactive.Banana.Frameworks as BF
+import Reflex
+import Reflex.Host.Class
+import Reflex.Host.Headless
 import System.Environment (getArgs)
 
 main :: IO ()
-main = do
+main = runHeadlessApp $ do
+  (myEvent, myTrigger) <- newTriggerEvent
+
+  let logValue val = putStrLn $ "Event fired: " ++ val
+  _ <- performEvent_ $ liftIO . logValue <$> myEvent
+
+  liftIO $ myTrigger "1"
+  liftIO $ myTrigger "2"
+  liftIO $ myTrigger "3"
+
+  let closeEvent = never
+  return closeEvent
+
+{-
+temp :: IO ()
+temp = do
+  args <- getArgs
+  case args of
+    [devicePath] -> do
+      realDev <- Evdev.newDevice $ BS.pack devicePath
+      Evdev.grabDevice realDev
+      putStrLn "Opened device:"
+      print realDev
+      virtDev <- virtualDevice
+      putStrLn "Swapping A and Z keys"
+
+      runHostFrame $ do
+        (eventTrigger, fireEvent) <- newTriggerEvent
+        let sendOutput :: Evdev.EventData -> IO ()
+            sendOutput = Uinput.writeEvent virtDev . swapKey
+        _ <- performEvent_ $ sendOutput <$> eventTrigger
+
+        forever $ do
+          (Evdev.Event eventData _) <- Evdev.nextEvent realDev
+          fireEvent eventData
+    _ -> putStrLn "Usage: Provide a device path such as /dev/input/eventX"
+-}
+{-
+oldmain :: IO ()
+oldmain = do
   args <- getArgs
   case args of
     [devicePath] -> do
@@ -37,6 +78,7 @@ networkDescription addHandler sendOutput = do
   let eOutput = swapKey <$> eInput
   let eIO = sendOutput <$> eOutput
   BF.reactimate eIO
+-}
 
 swapKey :: Evdev.EventData -> Evdev.EventData
 swapKey (Evdev.KeyEvent Codes.KeyA status) = Evdev.KeyEvent Codes.KeyZ status
