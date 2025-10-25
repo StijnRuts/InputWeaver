792479138b855997656512875806100006055789
Stijn Ruts <git@stijnruts.be>
2025-10-25 13:57:38 +0200

    WIP streamly

--- app/Main.hs
+++ app/Main.hs
@@ -7,9 +7,12 @@ import qualified Data.ByteString.Char8 as BS
 import Evdev (EventData (..))
 import qualified Evdev
 import qualified Evdev.Codes as Codes
+import qualified Evdev.Stream as ES
 import qualified Evdev.Uinput as Uinput
-import qualified Reflex as R
-import qualified Reflex.Host.Headless as RHH
+import qualified Streamly.Data.Stream.Prelude as S
+import qualified Streamly.Internal.Data.Stream as DS
+import Streamly.Internal.Data.Stream.IsStream as IS
+import Streamly.Internal.Data.Stream.Serial as Serial
 import System.Environment (getArgs)
 
 main :: IO ()
@@ -21,22 +24,15 @@ main = do
       Evdev.grabDevice realDev
       putStrLn "Opened device:"
       print realDev
+
       virtDev <- virtualDevice
       putStrLn "Swapping A and Z keys"
 
-      RHH.runHeadlessApp $ do
-        (myEvent, myTrigger) <- R.newTriggerEvent
-
-        let sendOutput = Uinput.writeEvent virtDev
-        _ <- R.performEvent_ $ liftIO . sendOutput . swapKey <$> myEvent
-
-        _ <- liftIO $ forkIO $ forever $ do
-          (Evdev.Event eventData _) <- Evdev.nextEvent realDev
-          myTrigger eventData
-
-        let closeEvent = R.never
-        return closeEvent
+      DS.drain $ S.mapM (Uinput.writeEvent virtDev . swapKey . getEventData) $ IS.fromSerial $ ES.readEvents realDev
     _ -> putStrLn "Usage: Provide a device path such as /dev/input/eventX"
+
+getEventData :: Evdev.Event -> Evdev.EventData
+getEventData (Evdev.Event eventData _) = eventData
 
 swapKey :: Evdev.EventData -> Evdev.EventData
 swapKey (Evdev.KeyEvent Codes.KeyA status) = Evdev.KeyEvent Codes.KeyZ status
@@ -49,31 +45,31 @@ virtualDevice =
     (BS.pack "haskell-uinput-example")
     Uinput.defaultDeviceOpts
       { Uinput.keys =
-          [ Codes.KeyA,
-            Codes.KeyB,
-            Codes.KeyC,
-            Codes.KeyD,
-            Codes.KeyE,
-            Codes.KeyF,
-            Codes.KeyG,
-            Codes.KeyH,
-            Codes.KeyI,
-            Codes.KeyJ,
-            Codes.KeyK,
-            Codes.KeyL,
-            Codes.KeyM,
-            Codes.KeyN,
-            Codes.KeyO,
-            Codes.KeyP,
-            Codes.KeyQ,
-            Codes.KeyR,
-            Codes.KeyS,
-            Codes.KeyT,
-            Codes.KeyU,
-            Codes.KeyV,
-            Codes.KeyW,
-            Codes.KeyX,
-            Codes.KeyY,
-            Codes.KeyZ
+          [ Codes.KeyA
+          , Codes.KeyB
+          , Codes.KeyC
+          , Codes.KeyD
+          , Codes.KeyE
+          , Codes.KeyF
+          , Codes.KeyG
+          , Codes.KeyH
+          , Codes.KeyI
+          , Codes.KeyJ
+          , Codes.KeyK
+          , Codes.KeyL
+          , Codes.KeyM
+          , Codes.KeyN
+          , Codes.KeyO
+          , Codes.KeyP
+          , Codes.KeyQ
+          , Codes.KeyR
+          , Codes.KeyS
+          , Codes.KeyT
+          , Codes.KeyU
+          , Codes.KeyV
+          , Codes.KeyW
+          , Codes.KeyX
+          , Codes.KeyY
+          , Codes.KeyZ
           ]
       }
--- devenv.nix
+++ devenv.nix
@@ -2,7 +2,7 @@
 {
   languages.haskell = {
     enable = true;
-    package = pkgs.haskell.compiler.ghc9103;
+    package = pkgs.haskell.compiler.ghc963;
   };
 
   packages = [
@@ -15,6 +15,9 @@
     "run:T420:keyboard".exec = "cabal exec example -- /dev/input/by-path/platform-i8042-serio-0-event-kbd";
     "run:T420:trackpad".exec = "cabal exec example -- /dev/input/by-path/platform-i8042-serio-1-event-mouse";
     "run:T420:trackpoint".exec = "cabal exec example -- /dev/input/by-path/platform-i8042-serio-2-event-mouse";
+    "run:P520:keyboard".exec = "cabal exec example -- /dev/input/by-id/usb-Dell_Dell_USB_Entry_Keyboard-event-kbd";
+    "run:P520:trackpad".exec = "cabal exec example -- /dev/input/by-id/usb-Apple_Inc._Magic_Trackpad_2_CC2101201T7J2Y1AA-event-mouse";
+    "run:P520:mouse".exec = "cabal exec example -- /dev/input/by-id/usb-Logitech_USB_Laser_Mouse-event-mouse";
     tests.exec = "cabal test";
     format.exec = "ormolu --mode inplace $(find {app,test} -name '*.hs')";
     lint.exec = "hlint {app,test}";
