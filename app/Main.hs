2dd44f331d03e83f4e7580bf1c56746e8be4dda5
Stijn Ruts <git@stijnruts.be>
2025-10-25 14:36:13 +0200

    Combine keyboard processing

--- app/Main.hs
+++ app/Main.hs
@@ -10,42 +10,30 @@ import qualified Evdev.Uinput as Uinput
 import System.Environment (getArgs)
 
 main :: IO ()
-main = delayExample
-
-readExample :: IO ()
-readExample = do
+main = do
   args <- getArgs
   case args of
     [devicePath] -> do
       realDev <- Evdev.newDevice $ BS.pack devicePath
+      Evdev.grabDevice realDev
       putStrLn "Opened device:"
       print realDev
+
+      virtDev <- virtualDevice
+      putStrLn "Swapping A and Z keys"
+
       forever $ do
-        ev <- Evdev.nextEvent realDev
-        print ev
+        evdevEvent <- Evdev.nextEvent realDev
+        Uinput.writeEvent virtDev $ mapKeyCode swapKey $ getEventData evdevEvent
+
     _ -> putStrLn "Usage: Provide a device path such as /dev/input/eventX"
 
-writeExample :: IO ()
-writeExample = do
-  virtDev <- virtualDevice
-  putStrLn "Pressing A:"
-  Uinput.writeEvent virtDev $ KeyEvent Codes.KeyA Pressed
-  Uinput.writeEvent virtDev $ SyncEvent Codes.SynReport
-  Uinput.writeEvent virtDev $ KeyEvent Codes.KeyA Released
-  Uinput.writeEvent virtDev $ SyncEvent Codes.SynReport
-  putStrLn "Done"
+getEventData :: Evdev.Event -> Evdev.EventData
+getEventData (Evdev.Event eventData _) = eventData
 
-swapExample :: IO ()
-swapExample = do
-  realDev <- Evdev.newDevice $ BS.pack "/dev/input/by-path/platform-i8042-serio-0-event-kbd"
-  Evdev.grabDevice realDev
-  virtDev <- virtualDevice
-  putStrLn "Swapping A and Z keys"
-  forever $ do
-    (Evdev.Event eventData eventTime) <- Evdev.nextEvent realDev
-    case eventData of
-      (Evdev.KeyEvent code val) -> Uinput.writeEvent virtDev $ KeyEvent (swapKey code) val
-      _ -> Uinput.writeEvent virtDev eventData
+mapKeyCode :: (Codes.Key -> Codes.Key) -> Evdev.EventData -> Evdev.EventData
+mapKeyCode f (Evdev.KeyEvent code val) = Evdev.KeyEvent (f code) val
+mapKeyCode _ eventData = eventData
 
 swapKey :: Codes.Key -> Codes.Key
 swapKey kc
